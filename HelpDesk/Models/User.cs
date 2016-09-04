5ac0facabde26e928f9e11ff4520dc822707288e
Kamil <kamil256@gmail.com>
2016-09-04 16:46:12 +0200

    Add user entity

---
+++ HelpDesk/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelpDesk.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string MobilePhone { get; set; }
+        public string Company { get; set; }
+        public string Department { get; set; }
+        public string City { get; set; }
+        public string Role { get; set; }
+    }
+}
