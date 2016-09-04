ae36927c6d9410a4771e872a5d8d1d90a20f46d1
Kamil <kamil256@gmail.com>
2016-09-04 18:35:11 +0200

    Modify User entity to add password and salt fields

--- HelpDesk/Models/User.cs
+++ HelpDesk/Models/User.cs
@@ -11,6 +11,8 @@ namespace HelpDesk.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
         public string Phone { get; set; }
         public string MobilePhone { get; set; }
         public string Company { get; set; }
