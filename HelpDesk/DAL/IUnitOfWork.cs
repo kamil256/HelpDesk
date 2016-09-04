8d66672e831ba8547fa0ace75777c0ea1db1ca91
Kamil <kamil256@gmail.com>
2016-09-04 17:47:11 +0200

    Add interface for unit of work

---
+++ HelpDesk/DAL/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using HelpDesk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelpDesk.DAL
+{
+    public interface IUnitOfWork
+    {
+        GenericRepository<User> UserRepository { get; }
+        GenericRepository<Ticket> TicketRepository { get; }
+        GenericRepository<Category> CategoryRepository { get; }
+        void Save();
+    }
+}
