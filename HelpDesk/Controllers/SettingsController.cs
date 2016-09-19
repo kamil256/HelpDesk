5e72c8e55c2f3f43372e8df52507a4621531e209
Kamil <kamil256@gmail.com>
2016-09-19 22:31:23 +0200

    Experimental

---
+++ HelpDesk/Controllers/SettingsController.cs
@@ -0,0 +1,27 @@
+﻿using HelpDesk.DAL;
+using HelpDesk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelpDesk.Controllers
+{
+    public class SettingsController : Controller
+    {
+        private IUnitOfWork unitOfWork;
+
+        public SettingsController()
+        {
+            unitOfWork = new UnitOfWork();
+        }
+
+        public ActionResult Index()
+        {
+            SettingsIndexViewModel model = new SettingsIndexViewModel();
+            model.Categories = unitOfWork.CategoryRepository.GetAll(filter: null, orderBy: q => q.OrderBy(c => c.Order)).ToList();
+            return View(model);
+        }
+    }
+}
---
+++ HelpDesk/Models/SettingsIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using HelpDesk.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelpDesk.Models
+{
+    public class SettingsIndexViewModel
+    {
+        public List<Category> Categories { get; set; }
+    }
+}
