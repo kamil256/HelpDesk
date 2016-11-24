e5fe6aa043b36ad4318c49c3303f25b82e179873
Kamil <kamil256@gmail.com>
2016-11-24 22:40:27 +0100

    Fix users Web API controller with its view model.

--- HelpDesk.UI/Controllers/WebAPI/UsersController.cs
+++ HelpDesk.UI/Controllers/WebAPI/UsersController.cs
@@ -44,138 +44,138 @@ namespace HelpDesk.UI.Controllers.WebAPI
 
         private IUnitOfWork unitOfWork;
 
-        public UsersController()//IUnitOfWork unitOfWork)
+        public UsersController()
         {
             this.unitOfWork = new UnitOfWork();
         }
 
         [OverrideAuthorization]
         [HttpGet]
-        public PagedUsers GetUsers([FromUri] UserFilteringModel model)
+        public HttpResponseMessage GetUsers(string role = null, string search = null, string sortBy = null, bool descSort = false, int page = 0)
         {
             List<Expression<Func<User, bool>>> filters = new List<Expression<Func<User, bool>>>();
-            if (!string.IsNullOrWhiteSpace(model.Search))
+
+            if (!string.IsNullOrEmpty(role))
             {
-                filters.Add(u => u.FirstName.ToLower().Contains(model.Search.ToLower()) ||
-                                 u.LastName.ToLower().Contains(model.Search.ToLower()) ||
-                                 u.Email.ToLower().Contains(model.Search.ToLower()) ||
-                                 u.Phone.ToLower().Contains(model.Search.ToLower()) ||
-                                 u.MobilePhone.ToLower().Contains(model.Search.ToLower()) ||
-                                 u.Company.ToLower().Contains(model.Search.ToLower()) ||
-                                 u.Department.ToLower().Contains(model.Search.ToLower()));
+                string roleId = RoleManager.FindByName(role).Id;
+                filters.Add(u => u.Roles.FirstOrDefault(r => r.RoleId == roleId) != null);
+            }
+
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+                filters.Add(u => u.FirstName.ToLower().Contains(search.ToLower()) ||
+                                 u.LastName.ToLower().Contains(search.ToLower()) ||
+                                 u.Email.ToLower().Contains(search.ToLower()) ||
+                                 u.Phone.ToLower().Contains(search.ToLower()) ||
+                                 u.MobilePhone.ToLower().Contains(search.ToLower()) ||
+                                 u.Company.ToLower().Contains(search.ToLower()) ||
+                                 u.Department.ToLower().Contains(search.ToLower()));
             }
 
             Func<IQueryable<User>, IOrderedQueryable<User>> orderBy = null;
 
-            switch (model.SortBy)
+            switch (sortBy)
             {
                 case "First name":
-                    if (model.DescSort)
+                    if (descSort)
                         orderBy = query => query.OrderByDescending(u => u.FirstName);
                     else
                         orderBy = query => query.OrderBy(u => u.FirstName);
                     break;
                 case "Email":
-                    if (model.DescSort)
+                    if (descSort)
                         orderBy = query => query.OrderByDescending(u => u.Email);
                     else
                         orderBy = query => query.OrderBy(u => u.Email);
                     break;
                 case "Phone":
-                    if (model.DescSort)
+                    if (descSort)
                         orderBy = query => query.OrderByDescending(u => u.Phone);
                     else
                         orderBy = query => query.OrderBy(u => u.Phone);
                     break;
                 case "Mobile phone":
-                    if (model.DescSort)
+                    if (descSort)
                         orderBy = query => query.OrderByDescending(u => u.MobilePhone);
                     else
                         orderBy = query => query.OrderBy(u => u.MobilePhone);
                     break;
                 case "Company":
-                    if (model.DescSort)
+                    if (descSort)
                         orderBy = query => query.OrderByDescending(u => u.Company);
                     else
                         orderBy = query => query.OrderBy(u => u.Company);
                     break;
                 case "Department":
-                    if (model.DescSort)
+                    if (descSort)
                         orderBy = query => query.OrderByDescending(u => u.Department);
                     else
                         orderBy = query => query.OrderBy(u => u.Department);
                     break;
                 case "Role":
-                    if (model.DescSort)
+                    if (descSort)
                         orderBy = query => query.OrderByDescending(u => u.Roles.FirstOrDefault().RoleId);
                     else
                         orderBy = query => query.OrderBy(u => u.Roles.FirstOrDefault().RoleId);
                     break;
                 case "Tickets":
-                    if (model.DescSort)
+                    if (descSort)
                         orderBy = query => query.OrderByDescending(u => u.CreatedTickets.Count);
                     else
                         orderBy = query => query.OrderBy(u => u.CreatedTickets.Count);
                     break;
                 case "Last name":
                 default:
-                    if (model.DescSort)
+                    if (descSort)
                         orderBy = query => query.OrderByDescending(u => u.LastName);
                     else
                         orderBy = query => query.OrderBy(u => u.LastName);
                     break;
-            }            
-
-            if (!string.IsNullOrEmpty(model.Role))
-            {
-                string roleId = RoleManager.FindByName(model.Role).Id;//unitOfWork.RoleRepository.Get().FirstOrDefault(r => r.Name == model.Role).Id;
-                filters.Add(u => u.Roles.FirstOrDefault(r => r.RoleId == roleId) != null);
             }
 
-            PagedUsers pagedUsers = new PagedUsers();
-            string adminRoleId = RoleManager.FindByName("Admin").Id;//unitOfWork.RoleRepository.Get().FirstOrDefault(r => r.Name == "Admin").Id;
-
-            int skip = 0;
-            int take = 0;
-
-            if (!model.IgnorePaging)
-            {
-                int usersPerPage = CurrentUser.Settings.UsersPerPage;
-                int numberOfUsers = UserManager.Users.Count();//unitOfWork.UserRepository.Get(filters: filters, orderBy: orderBy).Count();
-                int numberOfPages = (int)Math.Ceiling((decimal)numberOfUsers / usersPerPage);
-
-                skip = (model.Page - 1) * usersPerPage;
-                take = usersPerPage;
-
-                pagedUsers.NumberOfPages = numberOfPages;
-            }
+            int usersPerPage = CurrentUser.Settings.UsersPerPage;
+            int numberOfUsers = UserManager.Users.Count();
+            int numberOfPages;
 
             IQueryable<User> users = UserManager.Users;
+
             if (filters != null)
                 foreach (var filter in filters)
                     if (filter != null)
                         users = users.Where(filter);
+
             if (orderBy != null)
                 users = orderBy(users);
-            if (!model.IgnorePaging)
-            {
-                users = users.Skip(skip).Take(take);
-            }
-            pagedUsers.Users = users.Select(u => new UserDTO//unitOfWork.UserRepository.Get(filters: filters, orderBy: orderBy, skip: skip, take: take).Select(u => new UserDTO
-            {
-                UserId = u.Id,
-                FirstName = u.FirstName,
-                LastName = u.LastName,
-                Email = u.Email,
-                Phone = u.Phone,
-                MobilePhone = u.MobilePhone,
-                Company = u.Company,
-                Department = u.Department,
-                Role = u.Roles.FirstOrDefault(r => r.RoleId == adminRoleId) != null ? "Admin" : "User",
-                Tickets = u.CreatedTickets.Count
-            });         
 
-            return pagedUsers;
+            if (page != 0)
+            {
+                numberOfPages = (int)Math.Ceiling((decimal)numberOfUsers / usersPerPage);
+                users = users.Skip((page - 1) * usersPerPage).Take(usersPerPage);
+            }
+            else
+            {
+                numberOfPages = 1;
+            }
+
+            string adminRoleId = RoleManager.FindByName("Admin").Id;
+
+            return Request.CreateResponse(HttpStatusCode.OK, new
+            {
+                Users = users.Select(u => new UserDTO
+                {
+                    UserId = u.Id,
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    Email = u.Email,
+                    Phone = u.Phone,
+                    MobilePhone = u.MobilePhone,
+                    Company = u.Company,
+                    Department = u.Department,
+                    Role = u.Roles.FirstOrDefault(r => r.RoleId == adminRoleId) != null ? "Admin" : "User",
+                    Tickets = u.CreatedTickets.Count
+                }),
+                NumberOfPages = numberOfPages
+            });
         }
     }
 }
