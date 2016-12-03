8859df0397c7b73bfd187dcd3f53fb63dd354cb2
Kamil <kamil256@gmail.com>
2016-12-03 12:18:04 +0100

    Update users controller to handle errors and display success and fail messages.

--- HelpDesk.UI/Controllers/MVC/UsersController.cs
+++ HelpDesk.UI/Controllers/MVC/UsersController.cs
@@ -47,42 +47,53 @@ namespace HelpDesk.UI.Controllers.MVC
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Create([Bind(Include = "FirstName,LastName,Email,Password,ConfirmPassword,Phone,MobilePhone,Company,Department,Role")] CreateViewModel model)
         {
-            if (ModelState.IsValid)
-            {
-                User user = new User
-                {
-                    UserName = model.Email,
-                    Email = model.Email,
-                    FirstName = model.FirstName,
-                    LastName = model.LastName,
-                    Phone = model.Phone,
-                    MobilePhone = model.MobilePhone,
-                    Company = model.Company,
-                    Department = model.Department,
-                    Settings = new Settings()
-                };
-                IdentityResult createUserResult = await identityHelper.UserManager.CreateAsync(user, model.Password);
-                if (createUserResult.Succeeded)
-                {
-                    IdentityResult addUserToRoleResult;
-                    if (model.Role == "Admin")
-                        addUserToRoleResult = identityHelper.UserManager.AddToRole(user.Id, "Admin");
-                    else
-                        addUserToRoleResult = identityHelper.UserManager.AddToRole(user.Id, "User");
+            if (model.Role != "Admin" && model.Role != "User")
+                ModelState.AddModelError("Role", $"Role \"{model.Role}\" is incorrect.");
 
-                    if (addUserToRoleResult.Succeeded)
-                        return RedirectToAction("Index");
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    User user = new User
+                    {
+                        UserName = model.Email,
+                        Email = model.Email,
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        Phone = model.Phone,
+                        MobilePhone = model.MobilePhone,
+                        Company = model.Company,
+                        Department = model.Department,
+                        Settings = new Settings()
+                    };
+                    IdentityResult createUserResult = await identityHelper.UserManager.CreateAsync(user, model.Password);
+                    if (createUserResult.Succeeded)
+                    {
+                        IdentityResult addUserToRoleResult;
+                        if (model.Role == "Admin")
+                            addUserToRoleResult = identityHelper.UserManager.AddToRole(user.Id, "Admin");
+                        else
+                            addUserToRoleResult = identityHelper.UserManager.AddToRole(user.Id, "User");
+                        if (!addUserToRoleResult.Succeeded)
+                            TempData["Fail"] = "Unable to add user to selected role. Try again, and if the problem persists contact your system administrator.";
+
+                        TempData["Success"] = "Successfully created new user.";
+                        return RedirectToAction("Edit", new { id = user.Id });
+                    }
                     else
-                        foreach (string error in addUserToRoleResult.Errors)
+                        foreach (string error in createUserResult.Errors)
                             ModelState.AddModelError("", error);
                 }
-                else
-                    foreach (string error in createUserResult.Errors)
-                        ModelState.AddModelError("", error);
             }
+            catch
+            {
+                TempData["Fail"] = "Unable to create new user. Try again, and if the problem persists contact your system administrator.";
+            }
+
             return View(model);
         }
 
+        [OverrideAuthorization]
         [Authorize]
         public async Task<ActionResult> Edit(string id)
         {
@@ -91,10 +102,14 @@ namespace HelpDesk.UI.Controllers.MVC
             {
                 user = await identityHelper.UserManager.FindByIdAsync(id);
                 if (user == null)
+                {
+                    TempData["Fail"] = "Unable to display user details. Try again, and if the problem persists contact your system administrator.";
                     return RedirectToAction("Index");
+                }
             }
             else
                 user = identityHelper.CurrentUser;
+
             EditViewModel model = new EditViewModel
             {
                 UserId = user.Id,
@@ -112,6 +127,7 @@ namespace HelpDesk.UI.Controllers.MVC
 
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [OverrideAuthorization]
         [Authorize]
         public async Task<ActionResult> Edit([Bind(Include = "UserId,FirstName,LastName,Email,Phone,MobilePhone,Company,Department,Role")] EditViewModel model)
         {
@@ -120,44 +136,65 @@ namespace HelpDesk.UI.Controllers.MVC
             {
                 user = await identityHelper.UserManager.FindByIdAsync(model.UserId);
                 if (user == null)
+                {
+                    TempData["Fail"] = "Unable to edit user. Try again, and if the problem persists contact your system administrator.";
                     return RedirectToAction("Index");
+                }
+                if (model.Role != "Admin" && model.Role != "User")
+                    ModelState.AddModelError("Role", $"Role \"{model.Role}\" is incorrect.");
             }
             else
             {
                 user = identityHelper.CurrentUser;
                 ModelState.Remove("Role");
             }
-            if (ModelState.IsValid)
-            {
-                user.FirstName = model.FirstName;
-                user.LastName = model.LastName;
-                user.UserName = model.Email;
-                user.Email = model.Email;
-                user.Phone = model.Phone;
-                user.MobilePhone = model.MobilePhone;
-                user.Company = model.Company;
-                user.Department = model.Department;
 
-                IdentityResult userUpdateResult = await identityHelper.UserManager.UpdateAsync(user);
-                if (userUpdateResult.Succeeded)
+            try
+            {
+                if (ModelState.IsValid)
                 {
-                    if (identityHelper.IsCurrentUserAnAdministrator())
-                    {
-                        identityHelper.UserManager.RemoveFromRoles(user.Id, identityHelper.UserManager.GetRoles(user.Id).ToArray<string>());
-                        if (model.Role == "Admin")
-                            identityHelper.UserManager.AddToRole(user.Id, "Admin");
-                        else
-                            identityHelper.UserManager.AddToRole(user.Id, "User");
+                    user.FirstName = model.FirstName;
+                    user.LastName = model.LastName;
+                    user.UserName = model.Email;
+                    user.Email = model.Email;
+                    user.Phone = model.Phone;
+                    user.MobilePhone = model.MobilePhone;
+                    user.Company = model.Company;
+                    user.Department = model.Department;
+
+                    IdentityResult userUpdateResult = await identityHelper.UserManager.UpdateAsync(user);
+                    if (userUpdateResult.Succeeded)
+                    {                        
+                        if (identityHelper.IsCurrentUserAnAdministrator())
+                        {
+                            IdentityResult addUserToRoleResult = identityHelper.UserManager.RemoveFromRoles(user.Id, identityHelper.UserManager.GetRoles(user.Id).ToArray<string>());
+                            if (addUserToRoleResult.Succeeded)
+                            {
+                                if (model.Role == "Admin")
+                                    addUserToRoleResult = identityHelper.UserManager.AddToRole(user.Id, "Admin");
+                                else
+                                    addUserToRoleResult = identityHelper.UserManager.AddToRole(user.Id, "User");
+                            }
+                            if (!addUserToRoleResult.Succeeded)
+                                TempData["Fail"] = "Unable to edit user role. Try again, and if the problem persists contact your system administrator.";
+                        }
+
+                        TempData["Success"] = "Successfully edited user.";
+                        return RedirectToAction("Edit", new { id = user.Id });
                     }
-                    return RedirectToAction("Index");
+                    else
+                        foreach (string error in userUpdateResult.Errors)
+                            ModelState.AddModelError("", error);
                 }
-                else
-                    foreach (string error in userUpdateResult.Errors)
-                        ModelState.AddModelError("", error);
+            }
+            catch
+            {
+                TempData["Fail"] = "Unable to edit user. Try again, and if the problem persists contact your system administrator.";
             }
             return View(model);
         }
 
+        [OverrideAuthorization]
         [Authorize]
         public async Task<ActionResult> ChangePassword(string id)
         {
@@ -166,7 +203,10 @@ namespace HelpDesk.UI.Controllers.MVC
             {
                 user = await identityHelper.UserManager.FindByIdAsync(id);
                 if (user == null)
+                {
+                    TempData["Fail"] = "Unable to change user's password. Try again, and if the problem persists contact your system administrator.";
                     return RedirectToAction("Index");
+                }
             }
             else
                 user = identityHelper.CurrentUser;
@@ -181,6 +221,7 @@ namespace HelpDesk.UI.Controllers.MVC
 
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [OverrideAuthorization]
         [Authorize]
         public async Task<ActionResult> ChangePassword([Bind(Include = "UserId,CurrentPassword,NewPassword,ConfirmPassword")] ChangePasswordViewModel model)
         {
@@ -189,42 +230,38 @@ namespace HelpDesk.UI.Controllers.MVC
             {
                 user = await identityHelper.UserManager.FindByIdAsync(model.UserId);
                 if (user == null)
+                {
+                    TempData["Fail"] = "Unable to change user's password. Try again, and if the problem persists contact your system administrator.";
                     return RedirectToAction("Index");
+                }
             }
             else
-            {
                 user = identityHelper.CurrentUser;
-            }
 
-            //bool editingLoggedInUser = user.Email == User.Identity.Name;
-
-            //bool passwordIsCorrect = UserManager.CheckPassword(user, model.CurrentPassword);
-            //if (!passwordIsCorrect)
-            //    ModelState.AddModelError("CurrentPassword", "Incorrect current password.");
-
-
-            if (ModelState.IsValid)
+            try
             {
-                IdentityResult changePasswordResult = await identityHelper.UserManager.ChangePasswordAsync(user.Id, model.CurrentPassword, model.NewPassword);
-                if (changePasswordResult.Succeeded)
+                if (ModelState.IsValid)
                 {
-                    //if (editingLoggedInUser)
-                    //{
-                    //    IAuthenticationManager AuthManager = HttpContext.GetOwinContext().Authentication;
-                    //    AuthManager.SignOut();
-                    //    AuthManager.SignIn(await UserManager.CreateIdentityAsync(user, DefaultAuthenticationTypes.ApplicationCookie));
-                    //}
-                    //TempData["Success"] = "Successfully changed password!";
-                    return RedirectToAction("Index", "Home");
+                    IdentityResult changePasswordResult = await identityHelper.UserManager.ChangePasswordAsync(user.Id, model.CurrentPassword, model.NewPassword);
+                    if (changePasswordResult.Succeeded)
+                    {
+                        TempData["Success"] = "Successfully changed password!";
+                        return RedirectToAction("ChangePassword", new { id = user.Id });
+                    }
+                    else
+                        foreach (string error in changePasswordResult.Errors)
+                            ModelState.AddModelError("", error);
                 }
-                //else
-                //    foreach (string error in changePasswordResult.Errors)
-                //        ModelState.AddModelError("", error);
-
+            }
+            catch
+            {
+                TempData["Fail"] = "Unable to change user's password. Try again, and if the problem persists contact your system administrator.";
             }
             return View(model);
         }
 
+        [OverrideAuthorization]
+        [Authorize]
         public async Task<ActionResult> Tickets(string id)
         {
             User user;
@@ -232,15 +269,18 @@ namespace HelpDesk.UI.Controllers.MVC
             {
                 user = await identityHelper.UserManager.FindByIdAsync(id);
                 if (user == null)
+                {
+                    TempData["Fail"] = "Unable to list user's tickets. Try again, and if the problem persists contact your system administrator.";
                     return RedirectToAction("Index");
+                }
             }
             else
-            {
                 user = identityHelper.CurrentUser;
-            }
+
             return View("Tickets", (object)user.Id);
         }
 
+        [OverrideAuthorization]
         [Authorize]
         public async Task<ActionResult> History(string id)
         {
@@ -249,12 +289,13 @@ namespace HelpDesk.UI.Controllers.MVC
             {
                 user = await identityHelper.UserManager.FindByIdAsync(id);
                 if (user == null)
+                {
+                    TempData["Fail"] = "Unable to list user's tickets. Try again, and if the problem persists contact your system administrator.";
                     return RedirectToAction("Index");
+                }
             }
             else
-            {
                 user = identityHelper.CurrentUser;
-            }
 
             HistoryViewModel model = new HistoryViewModel
             {
@@ -278,27 +319,37 @@ namespace HelpDesk.UI.Controllers.MVC
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Delete(string userId)
         {
+            User user = await identityHelper.UserManager.Users.Include(u => u.CreatedTickets)
+                                                              .Include(u => u.RequestedTickets)
+                                                              .Include(u => u.AssignedTickets)
+                                                              .Include(u => u.Settings)
+                                                              .SingleOrDefaultAsync(u => u.Id == userId);
+            if (user == null)
             {
-                User user = await identityHelper.UserManager.Users.Include(u => u.CreatedTickets)
-                                                                  .Include(u => u.RequestedTickets)
-                                                                  .Include(u => u.AssignedTickets)
-                                                                  .Include(u => u.Settings)
-                                                                  .SingleOrDefaultAsync(u => u.Id == userId);
-                if (user == null)
-                    RedirectToAction("Index");
-
+                TempData["Fail"] = "Unable to delete user. Try again, and if the problem persists contact your system administrator.";
+                return RedirectToAction("Index");
+            }
+            
+            try
+            {
                 IdentityResult userDeleteResult = await identityHelper.UserManager.DeleteAsync(user);
 
                 if (userDeleteResult.Succeeded)
                 {
+                    TempData["Success"] = "Successfully deleted ticket.";
+
                     if (user.UserName == User.Identity.Name)
                     {
                         IAuthenticationManager AuthenticationManager = HttpContext.GetOwinContext().Authentication;
                         AuthenticationManager.SignOut();
                     }
-                }
-                return RedirectToAction("Index");
+                }                
             }
+            catch
+            {
+                TempData["Fail"] = "Unable to delete user. Try again, and if the problem persists contact your system administrator.";
+            }
+            return RedirectToAction("Index");
         }    
     }
 }
